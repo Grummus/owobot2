9c60a9eb46596fab05d29f8f309a66231457657e
Grummus <grahamklatt@msn.com>
2020-02-19 15:45:59 -0800

    make base bulgecount command file

---
+++ commands/info/bulgecount.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.connect(process.env.mongourl, {
+    useNewUrlParser: true
+});
+const Server = require('../../models/server.js');
+
+module.exports = {
+    name: "bulgecount",
+    category: "info",
+    description: "displays the number of bulges on a server.",
+    run: async (client, message, args) => {
+        //start making code you lazy shite
+    }
+}

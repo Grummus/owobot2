00c0b93bc610d0404ac3cc56d12e80bbbd485dfe
Grummus <grahamklatt@msn.com>
2020-02-27 16:11:03 -0800

    wrote vore class

---
+++ phrases/common/vore.js
@@ -0,0 +1,73 @@
+const Discord = require('discord.js');
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.connect(process.env.mongourl, {
+    useNewUrlParser: true
+});
+const Server = require('../../models/server.js');
+const GlobalData = require('../../models/globalData.js');
+
+
+module.exports = {
+    name: "vore",
+    category: "phrase",
+    description: "uuuuuuuuuuh",
+    run: async (client, message, args) => {
+        Server.findOne({
+            serverID: message.guild.id
+        }, (err, vores) => {
+            if(err) console.log(err);
+            if(!vores) {
+                const newServer = new Server({
+                    serverName: message.guild.name,
+                    serverID: message.guild.id,
+                    bulges: 0,
+                    vores: 1,
+                    lastVore: new Date()
+                })
+        
+                newServer.save().catch(err => console.log(err));
+                //increment number of servers
+                GlobalData.findOne({
+                    title: "Global Data"
+                }, (err, globalData) => {
+                    if(err) console.log(err);
+                    globalData.servers++;
+                    globalData.vores++;
+                    globalData.save().catch(err => console.log(err));
+                })
+            } else {
+                //increment the vores owo!
+                vores.vores = vores.vores + 1;
+
+                //calculate time owo
+                var startTime = vores.lastVore;
+                var endTime = new Date();
+                endTime -= startTime;
+                var seconds = endTime / 1000;
+                var days = Math.floor(seconds / (3600*24));
+                seconds  -= days*3600*24;
+                var hrs   = Math.floor(seconds / 3600);
+                seconds  -= hrs*3600;
+                var mnts = Math.floor(seconds / 60);
+                seconds  -= mnts*60;
+
+                message.reply('HAS SPOKEN THE FORBIDDEN WORD!\nThis server has gone:\n' + days + ' days,\n' + hrs + ' hours,\n' + mnts + ' minutes, and\n' + seconds + ' seconds without saying the forbidden word!');
+
+                vores.lastVore = new Date();
+                vores.save().catch(err => console.log(err));
+
+            }
+        });
+        //increment global vores owo! (mr. worldwide)
+        GlobalData.findOne({
+            title: "Global Data"
+        }, (err, globalData) => {
+            if(err) console.log(err);
+            globalData.vores++
+            globalData.lastVore = new Date();
+            globalData.save().catch(err => console.log(err));
+        })
+
+    }
+}

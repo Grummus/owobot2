cb92d4a11181d286423c3dfe6e181b777be95b33
Grummus <grahamklatt@msn.com>
2020-03-04 13:15:19 -0800

    changed logging to display server name

--- phrases/common/bulge.js
+++ phrases/common/bulge.js
@@ -14,7 +14,7 @@ module.exports = {
     run: async (client, message, args) => {
         //message.reply("OwO");
         message.channel.send("OwO");
-        console.log(`${message.author.username} said: ${message.content}`);
+        console.log(`${message.author.username} said: ${message.content} in server: ${message.guild.name}`);
         //query the database
         Server.findOne({
             serverID: message.guild.id

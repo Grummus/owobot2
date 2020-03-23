f97577621c4b7320039489500cbfa0a847491c65
Grummus <grahamklatt@msn.com>
2020-03-22 18:46:16 -0700

    added more info to webpage

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
-
+var mongoose = require('mongoose');
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
+
+mongoose.connect(process.env.mongourl, {
+    useNewUrlParser: true
+}, function(err) {
+    if(err) {
+        console.log(err);
+        console.log("==== ❌ oopsie woopsie, I failed to connect to the database uwu ====");
+        return process.exit();
+    }
+    console.log("✅ Connected to database!");
+});
+
+const GlobalData = require('./models/globalData.js');
+var globalBulgeCount;
+
+GlobalData.findOne({
+    title: "Global Data"
+}, (err, globalData) => {
+    if(err) console.log(err);
+    globalBulgeCount = globalData.bulges;
+})
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -15,7 +37,16 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
-	res.render('index');
+    res.render('index', {
+        globalBulgeCount: globalBulgeCount
+    });
+    
+});
+
+app.get('/bulgecount', function(req,res) {
+    res.render('bulgecount', {
+        globalBulgeCount: globalBulgeCount
+    });
 });
 
 app.listen(port, function() {

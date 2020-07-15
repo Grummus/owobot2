3b7d570e59eb76dea62ea5c6ea763ecd78a02741
Grummus <grahamklatt@msn.com>
2020-07-14 20:47:52 -0600

    Updated owohelp

--- commands/info/owohelp.js
+++ commands/info/owohelp.js
@@ -16,8 +16,9 @@ module.exports = {
         .setThumbnail(client.user.displayAvatarURL)
 
         embed.addField("Info:", "Hello, I am OwObot!\nI keep track of the bulges on your server so you don't have to!", true);
-        embed.addField("!bulgecount", "Displays the amount of bulges on your server");
-        embed.addField("!globalbulgecount", "Shows how many bulges I've seen on all the servers I'm in!~");
+        embed.addField(process.env.prefix + "bulgecount", "Displays the amount of bulges on your server");
+        embed.addField(process.env.prefix + "globalbulgecount", "Shows how many bulges I've seen on all the servers I'm in!~");
+        embed.addField(process.env.prefix + "leaderboard <bulges|forbiddenword>, Get a load of these degenerates!");
 
         message.channel.send(embed);
 

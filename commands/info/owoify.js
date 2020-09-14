2fd7398570744d39482149e885723e884f85f1d3
Grummus <grahamklatt@msn.com>
2020-09-14 13:47:23 -0700

    Made owoify case insensitive

--- commands/info/owoify.js
+++ commands/info/owoify.js
@@ -8,10 +8,13 @@ module.exports = {
     description: "OwOifies any text you send",
     supportedArgs: "<text>",
     run : async (client, message, args) => {
+        //turn args into single string
         let textIn = args.join(" ");
-        var newStr = textIn.replace(/r/g, "w");
-        newStr = newStr.replace(/n/g, "ny");
-        newStr = newStr.replace(/l/g, "w");
+
+        //replace letters
+        var newStr = textIn.replace(/r/gi, "w");
+        newStr = newStr.replace(/n/gi, "ny");
+        newStr = newStr.replace(/l/gi, "w");
 
         console.log("owoifying...");
         message.channel.send(newStr);

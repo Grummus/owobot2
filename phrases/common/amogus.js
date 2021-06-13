10405378a664462e9bff8eae08b43ff9377a01b9
Grummus <grahamklatt@msn.com>
2021-06-12 20:35:25 -0700

    added aliases to amogus detection

--- phrases/common/amogus.js
+++ phrases/common/amogus.js
@@ -4,8 +4,10 @@ module.exports = {
     name: "amogus",
     category: "phrase",
     description: "why",
+    aliases: ["sussy","sus", "amongus"],
     run: async (client, message, args) => {
         message.channel.send("stop. please, stop")
             .catch(err => console.log(err.message));
+        console.log(`Amogus detected in ${message.guild.name}`)
     }
 }

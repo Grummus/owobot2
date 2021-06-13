ca924746eb79f8d804e8c4e3bd32666c01bcaccb
Grummus <grahamklatt@msn.com>
2021-06-12 21:24:09 -0700

    added amongus easter egg

---
+++ commands/secret/amongus.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const Server = require('../../models/server.js');
+const fs = require('fs');
+
+module.exports = {
+    name: "amongus",
+    category: "secret",
+    description: "Secret, only usable by Grummus",
+    supportedArgs: "",
+    run: async (client, message, args) => {
+        let grummus = await client.users.fetch(168795588366696450);
+        if(message.author.id == 168795588366696450) {
+            if (message.member.voice.channel) {
+                const connection = await message.member.voice.channel.join();
+
+                // Create a dispatcher
+                const dispatcher = connection.play('audio/amongus.mp3',  { volume: 0.25 });
+
+                dispatcher.on('start', () => {
+                    console.log('amongus.mp3 is now playing!');
+                });
+
+                dispatcher.on('finish', () => {
+                    console.log('amongus.mp3 has finished playing!');
+                    dispatcher.destroy();
+                    connection.disconnect();
+                });
+
+                // Always remember to handle errors appropriately!
+                dispatcher.on('error', console.error);
+            } else {
+                message.channel.send("Sorry, you are not able to perform this command.");
+                console.log("message author was not Grummus");
+            }
+        }
+    }
+}

0224d25d677e9982e5280306e8427c93894dceac
Grummus <grahamklatt@msn.com>
2021-06-15 01:38:43 -0700

    changed status message

--- index.js
+++ index.js
@@ -55,7 +55,10 @@ client.on('ready', () => {
                 }
             });
         } else { */
-			client.user.setPresence({ activity: { name: `with ${globalData.bulges} bulges owo | !owohelp` }, status: 'online' })
+			// client.user.setPresence({ activity: { name: `with ${globalData.bulges} bulges owo | !owohelp` }, status: 'online' })
+			//	.catch(console.error);
+
+			client.user.setPresence({ activity: { name: 'y\'all know I can see your server names right? filthy degenerates | !owohelp' }, status: 'online' })
 				.catch(console.error);
 			// }
 		});
